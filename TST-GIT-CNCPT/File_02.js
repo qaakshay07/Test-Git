01389fd7c8a65e81e3e60c958990ecc9a1a35ce4
qaakshay07 <qaakshay07@gmail.com>
2026-02-23 07:57:01 +0530

    New changes moved with new branch

---
+++ TST-GIT-CNCPT/File_02.js
@@ -0,0 +1,2 @@
+console.log("Hi, Good morning Akshay!");
+console.log("This change is done with new branch named 'newbranch/feature02'");
---
+++ TST-GIT-CNCPT/File_03.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 5;
+
+function add(a,b)
+{
+    return a + b;
+    console.log(`The sum is ${a + b}`);
+}
